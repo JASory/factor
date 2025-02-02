01983d8dde43c5529fafa762b7d8bdaee3b86660
JASory <jasory@rust-cas.org>
2025-02-01 21:11:24 -0700

    Initial upload

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# factor
-Rust port of GNU factor for 128-bit integers
+
+  Rust port of GNU Factor for 128-bit integers. Some changes have been made to my preference
+  
+  1. Evaluation loop exits on the input of "q","quit" or "exit"
+  
+  2. Hexadecimal input is supported with the --hex option
+  
+  3. Factorisations are "properly" formatted using exponentiation and multiplication symbols
+  
+  Currently faster than the coreutils Rust port, and comparable to GNU factor  running in approximately 1/3t in the case of large semiprimes (the hardest case), although often slower for other "easier" composites.
+  
+  Most composites will be factored in less than 60 seconds (0.6s on average)
+
+  This performance is entirely due to the machine-factor library.
+  
+  This code is licensed under GPLv3. 
+
---
+++ src/main.rs
@@ -0,0 +1,151 @@
+use machine_factor::factorize_128;
+/*
+   --version
+   --help
+   --hex  Hexidecimal input
+*/
+static HELP : &str = "
+Usage: factor OPTION x1,x2,..
+
+  Produces prime factors for all the integers provided in the commandline
+  If no arguments are provided then integers are read from stdin until q,quit or exit is read.
+
+Options:
+
+  --hex         Reads input as hexidecimal
+  
+  --version     Returns the version and library about information
+  --help        This help page
+";
+
+static VERSION : &str = "
+factor 1.0
+License GPLv3+: GNU GPL version 3 or later <https://gnu.org/licenses/gpl.html>.
+This is free software: you are free to change and redistribute it.
+There is NO WARRANTY, to the extent permitted by law.
+
+Copyright (C) 2025 JASory.
+";
+
+enum Value{
+   Input(u128),
+   Error,
+   ParseError(String),
+   Quit,
+}
+
+fn format(x: u128) -> String{
+   let f = factorize_128(x);
+   
+   let mut output = String::new();
+      if f.powers[0] != 1{
+        output+=&(f.factors[0].to_string()+"^"+&f.powers[0].to_string());
+      }
+      else{
+        output+=&(f.factors[0].to_string());
+      }
+      if f.len > 1{
+      for i in 1..f.len{
+         if f.powers[i] != 1{
+         let pair = f.factors[i].to_string()+"^"+&f.powers[i].to_string();
+         output+=&(" * ".to_owned() + &pair);
+         }
+         else{
+         let pair = f.factors[i].to_string();
+         output+= &(" * ".to_owned()+&pair);
+         }
+      }
+      }
+      output
+  }
+
+fn read_integer(hex: bool) -> Value{
+   let mut input = String::new();
+   
+   match std::io::stdin().read_line(&mut input){
+     Ok(_) => (),
+     Err(_) => return Value::Error,
+   }
+   
+   let k = input.trim();
+   
+   if k == "q" || k == "quit" || k == "exit"{
+      return Value::Quit;
+   }
+   if hex {
+       match u128::from_str_radix(k,16){
+         Ok(x) => {return Value::Input(x);},
+         Err(_) => {return Value::ParseError(k.to_string());},
+       }
+   }
+   match k.parse::<u128>(){
+     Ok(x) => {return Value::Input(x);},
+     Err(_) => {return Value::ParseError(k.to_string());}
+   }
+}
+
+fn repl(hex: bool){
+       loop{
+          match read_integer(hex){
+            Value::Input(x) => println!("{}: {}",x,format(x)), 
+            Value::Error => println!("Unable to read from stdin"),
+            Value::ParseError(x) => println!("{} is not a valid input",x),
+            Value::Quit => break,
+          }
+       } 
+}
+
+fn main() {
+
+      let env_var = std::env::args().collect::<Vec<String>>();
+      let args_count = env_var.len()-1;
+      if args_count == 1{
+       if env_var[1] == "--hex"{
+          repl(true);   
+        }
+        else{
+          match env_var[1].parse::<u128>(){
+            Ok(x) => println!("{}: {}",x,format(x)),
+            Err(_) => {
+              if env_var[1] == "--help"{
+                 println!("{}",HELP);
+              }
+              if env_var[1] == "--version"{
+                println!("{}",VERSION);
+              }
+              else{
+                println!("Unrecognised option '{}'",env_var[1]);
+              }
+            },
+          }
+        }
+      }
+      // Zero arguments so we just run the repl
+      if args_count==0{
+        repl(false)
+      }
+      // This means that 
+      if args_count > 1{
+        let mut flag = false;
+        let mut start_point = 1;
+        if env_var[1] == "--hex"{
+           flag = true;
+           start_point = 2;
+        }
+        for i in env_var[start_point..].iter(){
+           if flag{
+              match u128::from_str_radix(i,16){
+               Ok(x) => println!("{}: {}",x,format(x)),
+               Err(_) => println!("{} is not a valid integer",i),
+              }
+           }
+           else{
+             match i.parse::<u128>(){
+               Ok(x) => println!("{}: {}",x,format(x)),
+               Err(_) => println!("{} is not a valid integer",i),
+             }
+           }
+        }
+      }
+
+}
